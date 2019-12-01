90333b1584f73862d6a990d65e97c0a6df31b230
Yukie <yukie.man97@gmail.com>
2019-12-01 05:11:50 -0800

    200- Errors

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/location")
+def location():
+    return render_template("location.html")
+
+@app.route("/socialMedia")
+def socialMedia():
+    return render_template("socialMedia.html")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+@app.route("/hobbs")
+def hobbs():
+    return render_template("hobbs.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
