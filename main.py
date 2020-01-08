3ca50e27bffcecbbdba99076f848716ae9c44c51
Yukie <yukie.man97@gmail.com>
2020-01-08 01:00:33 -0800

    Filled in Product Detail
    Also filled into Hobbies Tab

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
 @app.route("/hobbies")
 def hobbies():
     return render_template("hobbies.html")
@@ -17,6 +13,14 @@ def hobbies():
 @app.route("/location")
 def location():
     return render_template("location.html")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
